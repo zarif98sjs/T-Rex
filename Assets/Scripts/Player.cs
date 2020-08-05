e6ff4c6c8045dab632716e26cc3e4773d43a69b5
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2020-08-05 18:12:54 +0600

    Spawner Added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+
+
+    private Rigidbody2D rb ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rb.velocity = new Vector2(speed,rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpSpeed);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private float timeBetweenSpawn;
+    public float startTimeBetweenSpawn;
+    public float decreaseTime;
+    public float minTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBetweenSpawn <= 0f)
+        {
+            Instantiate(obstacle, transform.position , Quaternion.identity);
+            timeBetweenSpawn = startTimeBetweenSpawn;
+
+            if(startTimeBetweenSpawn > minTime)
+            {
+                startTimeBetweenSpawn -= decreaseTime;
+            }
+                
+        }
+        else
+        {
+            timeBetweenSpawn -= Time.deltaTime;
+        }
+    }
+}
